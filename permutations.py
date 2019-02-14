6c1d2884e8f495fc2e69ba1c7dbcbeddf9067de4
Ponkiruthika112 <46352265+Ponkiruthika112@users.noreply.github.com>
2019-02-15 00:31:28 +0800

    Update permutations.py

--- permutations.py
+++ permutations.py
@@ -1,6 +1,7 @@
 from itertools import permutations
 s=input()
-k=list(permutations(s))
+p=list(permutations(s))
+k=list(set(p))
 for i in range(0,len(k)):
         print("".join(k[i]))
 #permutation
