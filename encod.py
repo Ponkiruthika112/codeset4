a399121fbf7b9f0f08cd3d69881f45390085d4d1
Ponkiruthika112 <46352265+Ponkiruthika112@users.noreply.github.com>
2019-05-13 13:36:17 +0530

    Create encod.py

---
+++ encod.py
@@ -0,0 +1,20 @@
+# your code goes here
+s=input()
+i=0
+k=""
+j=i+1
+c=1
+while j<len(s):
+	if s[i]==s[j]:
+		c=c+1
+	else:
+		k=k+s[i]+str(c)
+		i=j
+		c=1
+ 
+	j=j+1
+k=k+s[i]+str(c)
+if s[len(s)-1]!=s[len(s)-2]:
+		k=k+s[len(s)-1]+str(1)
+print(k)
+#encoding
