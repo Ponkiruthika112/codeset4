5f164e2b6814ee51d36e123e45b47d5a23829f86
Ponkiruthika112 <46352265+Ponkiruthika112@users.noreply.github.com>
2019-05-14 22:56:38 +0800

    Update suffix_prefix.py

--- suffix_prefix.py
+++ suffix_prefix.py
@@ -8,9 +8,12 @@ for i in range(0,len(l)):
     p=p+l[i]
     k.append(p)
 d=k[::-1]
-for i in range(0,len(k)):
-    r=r+str(k[i]+d[i])+" "
+if len(k)==1:
+    r=str(l[0])
+else:
+    for i in range(0,len(k)):
+        r=r+str(k[i]+d[i])+" "
 print(r.strip())
-
+#v
 
 #suffix prefix
