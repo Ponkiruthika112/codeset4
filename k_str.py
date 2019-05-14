9106c040f941e062ca33452d74b704f0f51129b5
Ponkiruthika112 <46352265+Ponkiruthika112@users.noreply.github.com>
2019-05-14 13:45:50 +0530

    Create k_str.py

---
+++ k_str.py
@@ -0,0 +1,18 @@
+n,k=map(int,input().split())
+l=[]
+p=[]
+c=0
+for i in range(0,n):
+	s=input()
+	l.append(s)
+for i in range(0,len(l)-1):
+	if l[i]==l[i+1]:
+		c=c+1
+	else:
+		p.append(c)
+		c=0
+if p.count:
+	print("yes")
+else:
+	print("no")
+  #kstring
