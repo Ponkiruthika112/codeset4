1dd07103a5c3be7d7f41b3c38e5d37a2546d13c4
Ponkiruthika112 <46352265+Ponkiruthika112@users.noreply.github.com>
2019-05-14 16:16:37 +0530

    Create based_weights.py

---
+++ based_weights.py
@@ -0,0 +1,13 @@
+# your code goes here
+n=int(input())
+p=[]
+l=list(map(int,input().split()))
+k=list(map(int,input().split()))
+for i in range(0,len(k)):
+	p.append([l[i],k[i]])
+p.sort(key=lambda x:x[1],reverse=True)
+s=""
+for i in range(0,len(p)):
+	s=s+str(p[i][0])+" "
+print(s.strip())
+#based on weights ki
